7f74cae6e8833547369e2b5d4c822d84e448b478
Tony Khaov <tony.khaov@gmail.com>
2025-03-06 17:14:03 +0100

    feat: display 4 images

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,31 @@
-import { Text } from 'react-native'
+import { Image, Text, View, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import data from '../data.json'
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.images}>
+        {data[0].images.map((url) => (
+          <Image key={url} src={url} style={styles.image} />
+        ))}
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+  },
+  images: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 8,
+  },
+  image: {
+    width: '48%', // each image should be 50% width ideally but there is a gap and other spacings
+    height: 150,
+  },
+})
