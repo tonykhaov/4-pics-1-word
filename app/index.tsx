a1f130f9e83e1255301d2c65cdcf25c815f0e6fd
Tony Khaov <tony.khaov@gmail.com>
2025-03-06 17:24:11 +0100

    feat: display guess container box

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,26 @@
+import * as React from 'react'
 import { Image, Text, View, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import data from '../data.json'
 
 export default function HomeScreen() {
+  const [guess, setGuess] = React.useState<(string | null)[]>(
+    [...data[0].word].map((letter, i) => (i % 3 === 0 ? letter : null)),
+  )
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.images}>
         {data[0].images.map((url) => (
           <Image key={url} src={url} style={styles.image} />
+        ))}
+      </View>
+
+      <View style={styles.guess}>
+        {guess.map((letter, index) => (
+          <View key={index} style={styles.guessLetterBox}>
+            <Text style={styles.guessLetterBoxText}>{letter}</Text>
+          </View>
         ))}
       </View>
     </SafeAreaView>
@@ -19,7 +32,6 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   images: {
-    display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     gap: 8,
@@ -27,5 +39,29 @@ const styles = StyleSheet.create({
   image: {
     width: '48%', // each image should be 50% width ideally but there is a gap and other spacings
     height: 150,
+  },
+
+  guess: {
+    marginTop: 48,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+
+  guessLetterBox: {
+    backgroundColor: '#31363F',
+    minWidth: 48,
+    minHeight: 48,
+    maxHeight: 48,
+    maxWidth: 48,
+    borderBlockColor: '#76ABAE',
+    boxShadow: '0 0 0 1px #76ABAE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  guessLetterBoxText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    textTransform: 'uppercase',
   },
 })
